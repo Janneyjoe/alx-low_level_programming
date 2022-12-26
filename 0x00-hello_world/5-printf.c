035629ef5bdac90029b40b0694d44b6c22f797a7
Janneyjoe <joshuaasuquonya@gmail.com>
2022-12-25 17:57:59 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Succesful)
+ */
+int main(void)
+{
+	printf("\"Programming islike building a multilingual puzzle");
+	return (0);
+}
