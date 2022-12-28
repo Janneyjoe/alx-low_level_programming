fe65006bd85c475a42fd81923ba0b35163e45d72
Janneyjoe <joshuaasuquonya@gmail.com>
2022-12-28 09:17:45 -0800

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -10,8 +10,8 @@ int main(void)
 	char c;
 
 	c = 'z';
-	while 
-		(c >= 'z') {
+	while
+		(c >= 'a') {
 			putchar(c);
 			c--;
 		}
