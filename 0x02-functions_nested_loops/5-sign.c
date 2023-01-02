9526349784147c001fd3174486ff1906b6774207
Janneyjoe <joshuaasuquonya@gmail.com>
2023-01-02 15:13:52 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number of sign to be printed
- * return: 1 if number is greater than zero,
+ * Return: 1 if number is greater than zero,
  *
  * 0 of number is zero,
  * -1 if number is less than zero
